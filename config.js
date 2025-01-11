ea8a06e2c2b8404db9ca8436ac613d3039cc2fc3
SachinthaSandaru2 <realsachiya00@gmail.com>
2025-01-11 09:24:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ module.exports = {
     /**
      * owner details and parameters
      */
-    ownername: process.env.OWNER_NUMBER || process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
+    ownername: process.env.OWNER_NUMBER || process.env.Owner_Name || "REAL SACHIYA",
+    ownernumber: process.env.Owner_Number || "94767361910",
     instagramId: process.env.Insta || "3.69_pika",
 
     /**
